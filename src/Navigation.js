91f34f158b22310073119c8f70f72bc23492bc5a
Malin Hanak <malinehanak@gmail.com>
2018-09-20 18:26:37 +0200

    Dropdown finished

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,27 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Navigation.css';
 import CategoryListing from './CategoryListing';
+import BootstrapSort from './components/BootstrapSort'
 
 class Navigation extends Component {
-  constructor(){
-    super()
-    this.state = {
-      sortDrop: false,
-      filterDrop: false
-    }
-    this.handleSortDrop = this.handleSortDrop.bind(this);
-  }
-  handleSortDrop(){
-    this.state.sortDrop = !this.state.sortDrop;
-    console.log('Drop: ', this.state.sortDrop)
-  }
   render() {
     return(
       <nav>
-        <div onClick={this.handleSortDrop}>dropper</div>
         <Link to={`/`}>View all products</Link>
-        <Link to={`/sortDESC`}>Sort Price Desc</Link>
-        <Link to={`/sortASC`}>Sort Price Asc</Link>
+        <BootstrapSort/>
         <CategoryListing/>
       </nav>
     )
